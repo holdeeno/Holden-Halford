a9afd9fa5f725ab519ff81ca4661a78bef0d7be5
holdeeno <48695629+holdeeno@users.noreply.github.com>
2023-05-21 19:58:59 +0000

    Testing template setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ const Home: NextPage = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
-        <title>Next.js + TailwindCSS</title>
+        <title>Next.js + TailwindCSS Sample Project</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
