fdc9a7419a3d3bf8726e81917a8218ddeabc7996
holdeeno <halford.holden@gmail.com>
2023-07-04 19:37:17 +0000

    Added <Link> and rel="noopener noreferrer" to fix Vercel errors

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
+import Link from 'next/link'
 
 const Navigation: React.FC = () => {
   return (
     <nav className="flex flex-col sm:flex-row gap-2 justify-between">
       <div className="mb-6 sm:mb-8 md:mb-12">
-        <a href="/" className="text-decoration-none">
-          <h1 className="font-header text-2xl font-light mb-2">Holden Halford</h1>
-        </a>
+        <Link href="/">
+          <a className="text-decoration-none">
+            <h1 className="font-header text-2xl font-light mb-2">Holden Halford</h1>
+          </a>
+        </Link>
         <div className="flex gap-2 text-sm">
           <p>
-            <a className="underline" target="_blank" href="mailto:hello@holdenhalford.com?subject=Hey%20there!">Email</a>
+            <a className="underline" target="_blank" rel="noopener noreferrer" href="mailto:hello@holdenhalford.com?subject=Hey%20there!">Email</a>
           </p>
           <p>
-            <a className="underline" target="_blank" href="https://twitter.com/holdenhalford">Twitter</a>
+            <a className="underline" target="_blank" rel="noopener noreferrer" href="https://twitter.com/holdenhalford">Twitter</a>
           </p>
         </div>
       </div>
